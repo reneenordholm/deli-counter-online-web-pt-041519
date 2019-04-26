aba91cfa45b972a40d90463f32f980fcb6aba64b
Renee Nordholm <reneenordholm@gmail.com>
2019-04-26 12:29:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,1 @@
-def line(deli)
-  if deli.empty?
-    puts "The line is currently empty."
-  else 
-    current_line = "The line is currently: "
-    deli.each_with_index(1) { |name, index| current_line << "#{index}. #{name}"}
-    end
-    puts current_line
-  end
+current_line << "#{index}. #{name}"
